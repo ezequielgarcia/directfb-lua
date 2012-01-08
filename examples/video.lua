535b402130319118c0fee3656d9d7cf07e1f3d72
Ezequiel Garcia <elezegarcia@gmail.com>
2012-01-08 00:55:50 -0300

    Update video.lua example. Still is does not work

--- examples/video.lua
+++ examples/video.lua
@@ -2,22 +2,15 @@ require 'directfb'
 
 directfb.DirectFBInit()
 dfb = directfb.DirectFBCreate()
---dfb:SetCooperativeLevel('DFSCL_NORMAL')
 
 -- Get layer
 layer = dfb:GetDisplayLayer(0)
 
 video = dfb:CreateVideoProvider('/dev/video0')
---vsurf = dfb:CreateSurface(video:GetSurfaceDescription())
+vdesc = video:GetSurfaceDescription()
 
-desc = {}
-desc.posx = 0;
-desc.posy = 0;
-desc.width = 100
-desc.height = 100
-
-window  = layer:CreateWindow(desc)
+window  = layer:CreateWindow {width=vdesc.width, height=vdesc.height}
 wsurf = window:GetSurface()
 
 window:SetOpacity(0xff)
-video:PlayTo(wsurf, nil, 0, 0)	
+video:PlayTo(wsurf)
